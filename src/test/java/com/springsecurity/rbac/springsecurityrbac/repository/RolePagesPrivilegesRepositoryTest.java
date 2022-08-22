a35f2b9ef6fd41a78ade984934f42cbfff53d611
Amit Chavda <amitchavda9879@gmail.com>
2022-08-22 13:03:14 +0530

    Completed repository testing

--- src/test/java/com/springsecurity/rbac/springsecurityrbac/repository/RolePagesPrivilegesRepositoryTest.java
+++ src/test/java/com/springsecurity/rbac/springsecurityrbac/repository/RolePagesPrivilegesRepositoryTest.java
@@ -1,25 +1,136 @@
 package com.springsecurity.rbac.springsecurityrbac.repository;
 
-import com.springsecurity.rbac.springsecurityrbac.entity.security.RolePagesPrivileges;
-import org.junit.jupiter.api.BeforeEach;
+import com.springsecurity.rbac.springsecurityrbac.entity.contsants.PAGE;
+import com.springsecurity.rbac.springsecurityrbac.entity.contsants.PRIVILEGE;
+import com.springsecurity.rbac.springsecurityrbac.entity.security.*;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.when;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@TestPropertySource("/application-test.yml")
 public class RolePagesPrivilegesRepositoryTest {
 
     @Mock
     private RolePagesPrivilegesRepository repository;
 
-    private RolePagesPrivileges privileges;
+    @Test
+    void existByIdTest1() {
+        //Arrange
+        long roleId = 1L;
+        long pagesPrivilegesId = 1L;
+        boolean expected = true;
+        when(repository.existById(roleId, pagesPrivilegesId)).thenReturn(expected);
 
-    @BeforeEach
-    void setup() {
+        //Act
+        boolean actual = repository.existById(roleId, pagesPrivilegesId);
 
+        //Assert
+        assertEquals(actual, expected);
     }
 
+    @Test
+    void existByIdTest2() {
+        //Arrange
+        long roleId = 2L;
+        long pagesPrivilegesId = 1L;
+        boolean expected = false;
+        when(repository.existById(roleId, pagesPrivilegesId)).thenReturn(expected);
+
+        //Act
+        boolean actual = repository.existById(roleId, pagesPrivilegesId);
+
+        //Assert
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void findByIdTest1() {
+
+        //Arrange
+        Page rolePage = new Page();
+        rolePage.setId(1L);
+        rolePage.setName(PAGE.PRODUCT);
+
+
+        Privilege readPrivilege = new Privilege();
+        readPrivilege.setId(1L);
+        readPrivilege.setName(PRIVILEGE.READ);
+
+        PagesPrivileges pagesPrivileges = new PagesPrivileges();
+        pagesPrivileges.setId(1L);
+        pagesPrivileges.setPage(rolePage);
+        pagesPrivileges.setPrivilege(readPrivilege);
+
+        Role role = new Role();
+        role.setCreatedAt(LocalDateTime.now());
+        role.setId(1L);
+        role.setName("ADMIN");
+
+        RolePagesPrivileges expectedRolePagesPrivileges = new RolePagesPrivileges();
+
+        expectedRolePagesPrivileges.setPagesPrivileges(pagesPrivileges);
+        expectedRolePagesPrivileges.setRole(role);
+
+        long roleId = 1L;
+        long pagesPrivilegesId = 1L;
+        when(repository.findById(roleId, pagesPrivilegesId)).thenReturn(expectedRolePagesPrivileges);
+
+        //Act
+        RolePagesPrivileges actualRolePagesPrivileges = repository.findById(roleId, pagesPrivilegesId);
+
+        //Assert
+        assertEquals(actualRolePagesPrivileges, expectedRolePagesPrivileges);
+        assertEquals(actualRolePagesPrivileges.getPagesPrivileges(), pagesPrivileges);
+        assertEquals(actualRolePagesPrivileges.getPagesPrivileges().getPrivilege(), readPrivilege);
+        assertEquals(actualRolePagesPrivileges.getPagesPrivileges().getPage(), rolePage);
+        assertEquals(actualRolePagesPrivileges.getRole(), role);
+
+    }
+    @Test
+    void findByIdTest2() {
+
+        //Arrange
+        Page rolePage = new Page();
+        rolePage.setId(1L);
+        rolePage.setName(PAGE.PRODUCT);
+
+
+        Privilege readPrivilege = new Privilege();
+        readPrivilege.setId(1L);
+        readPrivilege.setName(PRIVILEGE.READ);
+
+        PagesPrivileges pagesPrivileges = new PagesPrivileges();
+        pagesPrivileges.setId(1L);
+        pagesPrivileges.setPage(rolePage);
+        pagesPrivileges.setPrivilege(readPrivilege);
+
+        Role role = new Role();
+        role.setCreatedAt(LocalDateTime.now());
+        role.setId(1L);
+        role.setName("ADMIN");
+
+        RolePagesPrivileges expectedRolePagesPrivileges = new RolePagesPrivileges();
+
+        expectedRolePagesPrivileges.setPagesPrivileges(pagesPrivileges);
+        expectedRolePagesPrivileges.setRole(role);
+
+        long roleId = 1L;
+        long pagesPrivilegesId = 1L;
+        when(repository.findById(roleId, pagesPrivilegesId)).thenReturn(expectedRolePagesPrivileges);
+
+        //Act
+        RolePagesPrivileges actualRolePagesPrivileges = repository.findById(roleId, 2L);
+
+        //Assert
+        assertNotEquals(actualRolePagesPrivileges, expectedRolePagesPrivileges);
+
+    }
 }
